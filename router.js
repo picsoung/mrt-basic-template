a1a7aa533c20114cf2ff9e043364c8d96b46b0fb
Nicolas Grenié <nicolas.grenie@gmail.com>
2014-04-04 12:16:20 -0700

    hello to home

--- router.js
+++ router.js
@@ -3,16 +3,11 @@ Router.configure({
 });
 
 Router.map(function () {
-  /**
-   * The route's name is "hello"
-   * The route's template is also "hello"
-   * The default action will render the hello template
-   */
-  this.route('hello', {
+  this.route('home', {
     path: '/',
-    template: 'hello',
+    template: 'home',
     onBeforeAction: function(){
-      Session.set('active', 'hello');
+      Session.set('active', 'home');
     }
 
   });
